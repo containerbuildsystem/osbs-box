e00047a7ffecbf86756fee39366123616080c802
Vadim Rutkovsky <vrutkovs@redhat.com>
2017-07-26 14:16:40 +0200

    Older docker won't allow specifying  "--format='{{.State.Running}}'" after container name

--- osbs-box.py
+++ osbs-box.py
@@ -46,8 +46,8 @@ def _run(cmd, ignore_exitcode=False, show_print=True):
 
 def _wait_until_container_is_up(container):
     container_is_up = True
-    cmd = ["docker", "inspect", "{0}_{1}_1".format(dir_path, container),
-           "--format='{{.State.Running}}'"]
+    cmd = ["docker", "inspect", "--format='{{.State.Running}}'",
+           "{0}_{1}_1".format(dir_path, container)]
     for attempts in range(0, 10):
         try:
             output = _run(cmd, show_print=False)
