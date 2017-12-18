51b57f122663729885baf755978340df8a0d76fe
Luiz Carvalho <lucarval@redhat.com>
2017-12-18 10:02:42 -0500

    Remove certificate generation from images
    Building and running container images for osbs-box will
    no longer generate certificates. Instead, this must be
    done by running generate-certs script. This has the main
    benefit of re-using certs between different intances of
    osbs-box. But it also provides a cleaner separation of
    concerns.

--- shared-data/bin/entrypoint.sh
+++ shared-data/bin/entrypoint.sh
@@ -4,11 +4,7 @@ set -xeuo pipefail
 # Expose generated local data in volume
 cp -r /opt/local/osbs /opt/
 cp -r /opt/local/koji-clients /opt/
-cp -r /opt/local/pki/koji /etc/pki/
 cp -r /opt/local/auth /
-if [ ! -f /certs/domain.crt ]; then
-  cp -r /opt/local/certs /
-fi
 
 for ip in `hostname -I`; do echo 'http://'$ip'/shared'; done
 exec "$@"
--- shared-data/bin/setup.sh
+++ shared-data/bin/setup.sh
@@ -3,53 +3,17 @@ set -xeuo pipefail
 
 # Create folder structure local data
 mkdir -p /opt/local/koji-clients
-mkdir -p /opt/local/pki/koji/{certs,private,confs}
-
-# Setup some root stuff
-chmod 600 /root/.pgpass
-chmod +x /usr/local/bin/*
-echo 'root:mypassword' | chpasswd
-
-# Generate CA certificate
-pushd /opt/local/pki/koji > /dev/null
-
-touch index.txt
-echo 01 > serial
-
-# CA
-conf=confs/ca.cnf
-cp ssl.cnf $conf
-
-openssl genrsa -out private/koji_ca_cert.key 2048
-openssl req -config $conf -new -x509 \
-    -subj "/C=US/ST=Drunken/L=Bed/O=IT/CN=koji-hub" \
-    -days 3650 \
-    -key private/koji_ca_cert.key \
-    -out koji_ca_cert.crt \
-    -extensions v3_ca
-
-cp private/koji_ca_cert.key private/kojihub.key
-cp koji_ca_cert.crt certs/kojihub.crt
-popd > /dev/null
 
 # Generate users certificates
 mkuser.sh kojiweb
 mkuser.sh kojibuilder
 mkuser.sh kojiadmin
 mkuser.sh kojiosbs
-chown -R nobody:nobody /opt/local/koji-clients
 
 # Enable kojiadmin config for root user
 mkdir -p /root/.koji
 ln -s /opt/local/koji-clients/kojiadmin/config /root/.koji/config
 
-# Generate certificates and set password for registry
-cat /opt/local/pki/koji/ssl.cnf | sed "s/email\:move/IP:172.17.0.1,email:move/"> ssl.cnf
-mkdir /opt/local/certs
-openssl req -config ssl.cnf \
-    -subj "/C=US/ST=Drunken/L=Bed/O=IT/CN=172.17.0.1" \
-    -newkey rsa:4096 -nodes -sha256 -keyout /opt/local/certs/domain.key \
-    -x509 -days 365 -out /opt/local/certs/domain.crt \
-    -extensions v3_ca
+# Set password for registry
 mkdir /opt/local/auth
 htpasswd -Bbn osbs craycray > /opt/local/auth/htpasswd
