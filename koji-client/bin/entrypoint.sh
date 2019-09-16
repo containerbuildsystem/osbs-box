096f16523591496cd49939b1305c7c259f34c4eb
Adam Cmiel <acmiel@redhat.com>
2019-09-16 13:45:22 +0200

    Ep. II: Attack of the Client
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

---
+++ koji-client/bin/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -euo pipefail
+
+# Become kojiadmin
+pick-koji-user.sh "kojiadmin"
+
+# Wait for up to a minute until koji is ready
+if ! timeout 60 moshimoshi.sh; then
+    echo "Koji still not ready after 60s, something might be wrong." >&2
+    exit 124
+fi
+
+exec "$@"
