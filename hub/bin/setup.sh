f50688c5303b594ee76e1db3cfceee6b7702a190
Adam Cmiel <acmiel@redhat.com>
2019-09-17 10:13:36 +0200

    Ep. V: The Hub Strikes Back
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- hub/bin/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -xeuo pipefail
-
-# Use kojiadmin user by default
-mkdir -p /root/.koji
-ln -fs /opt/koji-clients/kojiadmin/config /root/.koji/config
---
+++ koji-hub/bin/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -euo pipefail
+
+# Create basic directory structure for koji and change the owner to apache
+# Has to be done at runtime because the /mnt/koji/ directory is mounted
+mkdir -p /mnt/koji/{packages,repos,work,scratch}
+chown -R apache:apache /mnt/koji
+
+exec "$@"
